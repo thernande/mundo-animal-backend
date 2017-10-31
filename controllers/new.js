55b5e29fbb4b2f98ba6c09699e413b46e2fd4de1
thernande <tehp1993@gmail.com>
2017-10-31 14:49:12 -0500

    se reusa codigo para hacer las funciones de veterinary y news y se crea la funcion de borrar imagenes

--- controllers/new.js
+++ controllers/new.js
@@ -15,6 +15,210 @@ function pruebas(req, res){
 	});
 }
 
+function saveNews(req, res){
+	var news = new New;
+
+	var params = req.body;
+
+	if(!params){
+		news.name = params.name;
+		news.description = params.description;
+		news.date = params.date;
+		news.image = null;
+		news.user = req.user.sub;
+
+		news.save((err, newsStored) => {
+			if(err){
+				res.status(500).send({ error: 'Something failed!' });
+			}
+			else{
+				if(!newsStored){
+					res.status(500).send({ message: "no se ha guardado la noticia" });
+				}
+				else
+				{
+					res.status(200).send({newsStored});
+				}
+			}
+		});
+	}
+	else
+	{
+		res.status(200).send({message: "there isn't params to store"});
+	}
+
+	//res.status(200).send({message: "Probando el metodo de guardar"})
+}
+
+function getNews(req, res){
+	News.find({}).populate({path: 'user'}).exec((err, news) =>{
+		if(err){
+				res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!news){
+				res.status(404).send({ error: 'no se pudo encontrar noticias' });
+			}
+			{
+				res.status(200).send({news});
+			}
+		}
+	});
+}
+
+function getNew(req, res){
+	var newId = req.params.id;
+	New.findById(newId).populate({path: 'user'}).exec((err, news) => {
+		if(err){
+				res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!news){
+				res.status(404).send({ error: 'no se pudo encontrar la noticia' });
+			}
+			{
+				res.status(200).send({news});
+			}
+		}
+	})
+}
+
+function updateNews(req, res){
+	var newsId = req.params.id;
+	var update = req.body;
+
+	News.findByIdAndUpdate(newsId, update, {new:true}, (err, newsUpdate) =>{
+		if(err){
+			res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!newsUpdate){
+				res.status(404).send({ error: 'no se pudo actualizar el noticia' });
+			}
+			else
+			{
+				res.status(200).send({newsUpdate});
+			}
+		}
+	})
+}
+
+function uploadImage(req, res){
+	var newsId = req.params.id;
+	var file_name = 'empty';
+	//console.log(req.files);
+	if(req.files){
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('/');
+		file_name = file_split[2];
+
+		var ext_split = file_name.split('.');
+		var file_ext = ext_split[1];
+		
+		if(file_ext == "png" || file_ext == "jpg" || file_ext == "jpeg" || file_ext == "gif"){
+			
+			/*if(animalId != req.animal.sub){
+				return res.status(404).send({
+					message: "no tienes permiso para actualizar el animal"
+				})
+			}*/
+
+			News.findByIdAndUpdate(newsId, {image: file_name}, {new:true}, (err, newsUpdate) =>{
+				if (err) {
+					res.status(500).send({message:'Error al actualizar la noticia'});
+				}else{
+					if(!newsUpdate){
+						res.status(404).send({message:'no se ha podido actualizar la noticia'});
+					}else{
+						res.status(200).send({news: newsUpdate, image: file_name});
+					}
+				}
+			});
+
+		}else{
+			fs.unlink(file_path, (err) =>{
+				if (err) {
+					res.status(200).send({message:'invalid form and file erase'});
+				}
+				else{
+					res.status(200).send({message:'invalid form'});
+				}
+			});
+		}
+		
+	}else{
+		res.status(404).send({message:'the image has not been upload'});
+	}
+}
+
+function deleteImage(req,res){
+	var image = req.params.image;
+	var file_path = "uploads/news/"+image;
+	if(req.params.image == null){
+		res.status(404).send({message: 'there is not image to delete'});
+	}
+	else
+	{
+		fs.unlink(file_path, (err) =>{
+			if (err) {
+				res.status(200).send({message:'the file was not delete'});
+			}
+			else{
+				res.status(200).send({message:'file erase correctly'});
+			}
+		});
+	}
+}
+
+function getImageFile(req, res){
+	var imageFile = req.params.imageFile;
+	var pathFile = './uploads/news/'+imageFile;
+
+	fs.exists(pathFile, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(pathFile));
+		}else
+		{
+			res.status(404).send({message:'the image doesnt exists'});
+		}
+		
+	});
+	//res.status(200).send({message:'get image file'});
+}
+
+function deleteNews(req, res){
+	var newsId = req.params.id;
+
+	News.findByIdAndRemove(newsId, (err, newsRemove) => {
+		if(err){
+			res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!newsRemove){
+				res.status(404).send({ error: 'no se ha podido borrar la noticia ' });
+			}
+			else
+			{
+				res.status(200).send({newsRemove});
+			}
+		}
+	});
+}
+
+
 module.exports = {
-	pruebas
+	pruebas,
+	saveNews,
+	getNews,
+	getNew,
+	updateNews,
+	uploadImage,
+	getImageFile,
+	deleteNews,
+	deleteImage
+
 }
--- controllers/veterinary.js
+++ controllers/veterinary.js
@@ -15,6 +15,209 @@ function pruebas(req, res){
 	});
 }
 
+function saveVeterinary(req, res){
+	var veterinary = new Veterinary;
+
+	var params = req.body;
+
+	if(!params){
+		veterinary.name = params.name;
+		veterinary.direction = params.description;
+		veterinary.phone = params.date;
+		veterinary.image = null;
+		veterinary.email = params.email;
+
+		veterinary.save((err, veterinaryStored) => {
+			if(err){
+				res.status(500).send({ error: 'Something failed!' });
+			}
+			else{
+				if(!veterinaryStored){
+					res.status(500).send({ message: "no se ha guardado la noticia" });
+				}
+				else
+				{
+					res.status(200).send({veterinaryStored});
+				}
+			}
+		});
+	}
+	else
+	{
+		res.status(200).send({message: "there isn't params to store"});
+	}
+
+	//res.status(200).send({message: "Probando el metodo de guardar"})
+}
+
+function getVeterinaries(req, res){
+	Veterinaries.find({}).populate({path: 'user'}).exec((err, veterinaries) =>{
+		if(err){
+				res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!veterinaries){
+				res.status(404).send({ error: 'no se pudo encontrar veterinarias' });
+			}
+			{
+				res.status(200).send({veterinaries});
+			}
+		}
+	});
+}
+
+function getVeterinary(req, res){
+	var veterinaryId = req.params.id;
+	Veterinary.findById(veterinaryId).populate({path: 'user'}).exec((err, veterinary) => {
+		if(err){
+				res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!veterinary){
+				res.status(404).send({ error: 'no se pudo encontrar la noticia' });
+			}
+			{
+				res.status(200).send({veterinary});
+			}
+		}
+	})
+}
+
+function updateVeterinary(req, res){
+	var veterinaryId = req.params.id;
+	var update = req.body;
+
+	Veterinary.findByIdAndUpdate(veterinaryId, update, {new:true}, (err, veterinaryUpdate) =>{
+		if(err){
+			res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!veterinaryUpdate){
+				res.status(404).send({ error: 'no se pudo actualizar el noticia' });
+			}
+			else
+			{
+				res.status(200).send({veterinaryUpdate});
+			}
+		}
+	})
+}
+
+function uploadImage(req, res){
+	var veterinaryId = req.params.id;
+	var file_name = 'empty';
+	//console.log(req.files);
+	if(req.files){
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('/');
+		file_name = file_split[2];
+
+		var ext_split = file_name.split('.');
+		var file_ext = ext_split[1];
+		
+		if(file_ext == "png" || file_ext == "jpg" || file_ext == "jpeg" || file_ext == "gif"){
+			
+			/*if(animalId != req.animal.sub){
+				return res.status(404).send({
+					message: "no tienes permiso para actualizar el animal"
+				})
+			}*/
+
+			Veterinary.findByIdAndUpdate(veterinaryId, {image: file_name}, {new:true}, (err, veterinaryUpdate) =>{
+				if (err) {
+					res.status(500).send({message:'Error al actualizar la noticia'});
+				}else{
+					if(!veterinaryUpdate){
+						res.status(404).send({message:'no se ha podido actualizar la noticia'});
+					}else{
+						res.status(200).send({veterinary: veterinaryUpdate, image: file_name});
+					}
+				}
+			});
+
+		}else{
+			fs.unlink(file_path, (err) =>{
+				if (err) {
+					res.status(200).send({message:'invalid form and file erase'});
+				}
+				else{
+					res.status(200).send({message:'invalid form'});
+				}
+			});
+		}
+		
+	}else{
+		res.status(404).send({message:'the image has not been upload'});
+	}
+}
+
+function deleteImage(req,res){
+	var image = req.params.image;
+	var file_path = "uploads/veterinaries/"+image;
+	if(req.params.image == null){
+		res.status(404).send({message: 'there is not image to delete'});
+	}
+	else
+	{
+		fs.unlink(file_path, (err) =>{
+			if (err) {
+				res.status(200).send({message:'the file was not delete'});
+			}
+			else{
+				res.status(200).send({message:'file erase correctly'});
+			}
+		});
+	}
+}
+
+function getImageFile(req, res){
+	var imageFile = req.params.imageFile;
+	var pathFile = './uploads/veterinaries/'+imageFile;
+
+	fs.exists(pathFile, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(pathFile));
+		}else
+		{
+			res.status(404).send({message:'the image doesnt exists'});
+		}
+		
+	});
+	//res.status(200).send({message:'get image file'});
+}
+
+function deleteVeterinary(req, res){
+	var veterinaryId = req.params.id;
+
+	Veterinary.findByIdAndRemove(veterinaryId, (err, veterinaryRemove) => {
+		if(err){
+			res.status(500).send({ error: 'Something failed!' });
+		}
+		else
+		{
+			if(!veterinaryRemove){
+				res.status(404).send({ error: 'no se ha podido borrar la noticia ' });
+			}
+			else
+			{
+				res.status(200).send({veterinaryRemove});
+			}
+		}
+	});
+}
+
 module.exports = {
-	pruebas
+	pruebas,
+	saveVeterinary,
+	updateVeterinary,
+	getVeterinary,
+	getVeterinaries,
+	deleteVeterinary,
+	getImageFile,
+	uploadImage,
+	deleteVeterinary
+
 }
